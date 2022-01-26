619a19748632da19d50fb7736424e88c0f093182
Tim Hunter <thunte27@uwo.ca>
2022-01-25 23:19:14 -0500

    Add development page

---
+++ pages/dev.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function dev() {
+  return (
+    <div>
+      <h1>Development</h1>
+      <h2>Here is a sampling of my various development projects</h2>
+    </div>
+  );
+}
+
+export default dev;
