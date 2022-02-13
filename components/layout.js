68919a52d20477dc5325c68c6fbe0a45a5aee477
Tim Hunter <thunte27@uwo.ca>
2022-02-13 01:04:05 -0500

    Add drawer navbar with animations

--- components/layout.js
+++ components/layout.js
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { FaBars } from "react-icons/fa";
 
 import Navbar from "./navbar";
 
 const Layout = ({ children }) => {
-  const [displayNav, setDisplayNav] = useState(false);
-
   return (
     <>
-      <header>
-        <div>
-          <div className="nav-btn" onClick={() => setDisplayNav(!displayNav)}>
-            <FaBars />
-          </div>
-          <h1>TMHNTR</h1>
-        </div>
-        {displayNav && <Navbar />}
-      </header>
+      <Navbar />
       <main>{children}</main>
     </>
   );
--- components/navbar.js
+++ components/navbar.js
@@ -1,33 +1,64 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaCode, FaHome } from "react-icons/fa";
+import { MdOutlineScience, MdInfoOutline } from "react-icons/md";
 
-function Navbar() {
+const NavItem = ({ icon, href, label, onClick }) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/research">
-            <a>Research</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/development">
-            <a>Development</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <li>
+      <Link href={href}>
+        <a onClick={() => onClick()}>
+          {icon} {label}
+        </a>
+      </Link>
+    </li>
   );
-}
+};
+
+const Navbar = (props) => {
+  const [displayNav, setDisplayNav] = useState(false);
+
+  return (
+    <>
+      <header>
+        <div className="nav-btn" onClick={() => setDisplayNav(!displayNav)}>
+          <FaBars />
+        </div>
+        <h2>TMHNTR</h2>
+        <nav className={displayNav && "nav-visible"}>
+          <ul>
+            <NavItem
+              icon={<FaHome />}
+              href="/"
+              label="home"
+              onClick={() => setDisplayNav(false)}
+            />
+            <NavItem
+              icon={<MdInfoOutline />}
+              href="/about"
+              label="about"
+              onClick={() => setDisplayNav(false)}
+            />
+            <NavItem
+              icon={<MdOutlineScience />}
+              href="/research"
+              label="research"
+              onClick={() => setDisplayNav(false)}
+            />
+            <NavItem
+              icon={<FaCode />}
+              href="/development"
+              label="development"
+              onClick={() => setDisplayNav(false)}
+            />
+          </ul>
+        </nav>
+        {displayNav && (
+          <div id="nav-backdrop" onClick={() => setDisplayNav(false)}></div>
+        )}
+      </header>
+    </>
+  );
+};
 
 export default Navbar;
