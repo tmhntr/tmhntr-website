80134b534f43cb1e099194e63dbef07f0e65a7c9
Tim Hunter <thunte27@uwo.ca>
2022-03-30 09:25:30 -0400

    Add working email servise

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = {
 
     return config;
   },
+  env: {
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+  },
 };
